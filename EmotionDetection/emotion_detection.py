613c84c3c0cb2e8e18f4f0e77f7497668a920668
krt-c <nimsolo@gmail.com>
2026-04-02 15:02:52 -0400

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,21 +7,32 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    emotion_predictions = formatted_response['emotionPredictions'][0]
-    emotion_predictions_emotions = emotion_predictions['emotion']
 
-    anger_score = emotion_predictions_emotions['anger']
-    disgust_score = emotion_predictions_emotions['disgust']
-    fear_score = emotion_predictions_emotions['fear']
-    joy_score = emotion_predictions_emotions['joy']
-    sadness_score = emotion_predictions_emotions['sadness']
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotion_predictions = formatted_response['emotionPredictions'][0]
+        emotion_predictions_emotions = emotion_predictions['emotion']
 
-    dominant_emotion = max(emotion_predictions_emotions, key=emotion_predictions_emotions.get)
+        anger_score = emotion_predictions_emotions['anger']
+        disgust_score = emotion_predictions_emotions['disgust']
+        fear_score = emotion_predictions_emotions['fear']
+        joy_score = emotion_predictions_emotions['joy']
+        sadness_score = emotion_predictions_emotions['sadness']
+
+        dominant_emotion = max(emotion_predictions_emotions, key=emotion_predictions_emotions.get)
     
-    return {'anger': anger_score,
-            'disgust': disgust_score,
-            'fear': fear_score,
-            'joy': joy_score,
-            'sadness': sadness_score,
-            'dominant_emotion': dominant_emotion}
+        return {'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion': dominant_emotion}
+
+    elif response.status_code == 400:
+        
+        return {'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None,
+                'dominant_emotion': None}
--- server.py
+++ server.py
@@ -8,14 +8,18 @@ def post_emotion_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
-    anger_score = response['anger']
-    disgust_score = response['disgust']
-    fear_score = response['fear']
-    joy_score = response['joy']
-    sadness_score = response['sadness']
-    dominant_emotion = response['dominant_emotion']
+    if(response['dominant_emotion'] == None):
+        return "Invalid text! Please try again!"
 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion)
+    else:
+        anger_score = response['anger']
+        disgust_score = response['disgust']
+        fear_score = response['fear']
+        joy_score = response['joy']
+        sadness_score = response['sadness']
+        dominant_emotion = response['dominant_emotion']
+
+        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion)
 
 
 @app.route("/") 
